63bf4f5b881b206dc9af0dcfbb9676c4ada85915
age7359 <age7359@g.rit.edu>
2024-01-19 16:35:57 -0500

    Grass Toucher Skeleton Code

---
+++ Assets/Scripts/GrassToucher.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassToucher : Enemy
+{
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool canAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool IsKnockedOut()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.koTime = 8f;
+        base.attackCooldown = 4f;
+        base.damage = 20;
+        base.moveSpeed = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
