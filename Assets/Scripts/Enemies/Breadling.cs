335637e8194d502c1d7a2902985839122a0361d6
age7359 <age7359@g.rit.edu>
2024-01-19 16:48:18 -0500

    Skeleton Implementations of the other basic enemies
    +Breadling
    +RoyalBaker
    +Teleporter
    +Wrestler

--- Assets/Scripts/Enemies/Breadling.cs
+++ Assets/Scripts/Enemies/Breadling.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breadling : MonoBehaviour
+public class Breadling : Enemy
 {
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool canAttack()
+    {
+        return false;
+    }
+
+    public override bool IsKnockedOut()
+    {
+        return false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.koTime = 8f;
+        base.attackCooldown = 4f;
+        base.damage = 20;
+        base.moveSpeed = 60f;
     }
 
     // Update is called once per frame
--- Assets/Scripts/Enemies/Wrestler.cs
+++ Assets/Scripts/Enemies/Wrestler.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wrestler : MonoBehaviour
+public class Wrestler : Enemy
 {
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool canAttack()
+    {
+        return false;
+    }
+
+    public override bool IsKnockedOut()
+    {
+        return false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.koTime = 10f;
+        base.attackCooldown = 5f;
+        base.damage = 100;
+        base.moveSpeed = 30f;
     }
 
     // Update is called once per frame
